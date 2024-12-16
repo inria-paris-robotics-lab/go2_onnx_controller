c23c0401d3c1d2629719a3a7e2f0b388ab3aba50
ubgk <ubgk@users.noreply.github.com>
2024-12-16 14:11:17 +0100

    fix so name for macos

--- onnx_inference/cmake/dependencies.cmake
+++ onnx_inference/cmake/dependencies.cmake
@@ -8,12 +8,14 @@ macro(onnx_dependencies)
             DOWNLOAD_EXTRACT_TIMESTAMP FALSE
             URL https://github.com/microsoft/onnxruntime/releases/download/v1.20.1/onnxruntime-linux-x64-1.20.1.tgz
         )
+        set(LIBRARY_NAME "libonnxruntime.so")
     elseif(APPLE)
         FetchContent_Declare(
             onnxruntime
             DOWNLOAD_EXTRACT_TIMESTAMP FALSE
             URL https://github.com/microsoft/onnxruntime/releases/download/v1.20.1/onnxruntime-osx-universal2-1.20.1.tgz
         )
+        set(LIBRARY_NAME "libonnxruntime.dylib")
     endif()
 
     FetchContent_MakeAvailable(onnxruntime)
@@ -22,10 +24,10 @@ macro(onnx_dependencies)
     # Include path: ${onnxruntime_SOURCE_DIR}/include/
     add_library(onnxruntime SHARED IMPORTED)
     set_target_properties(onnxruntime PROPERTIES
-        IMPORTED_LOCATION ${onnxruntime_SOURCE_DIR}/lib/libonnxruntime.so
+        IMPORTED_LOCATION ${onnxruntime_SOURCE_DIR}/lib/${LIBRARY_NAME}
         INTERFACE_INCLUDE_DIRECTORIES ${onnxruntime_SOURCE_DIR}/include/
     )
     # Install the library
-    file(GLOB ONNXRUNTIME_LIBS "${onnxruntime_SOURCE_DIR}/lib/libonnxruntime.so*")
+    file(GLOB ONNXRUNTIME_LIBS "${onnxruntime_SOURCE_DIR}/lib/libonnxruntime.*")
     install(FILES ${ONNXRUNTIME_LIBS} DESTINATION lib)
 endmacro()
