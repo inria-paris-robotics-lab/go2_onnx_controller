e1c9db2fcc0f6c959c2cce6254d1b999017b40b6
ubgk <ubgk@users.noreply.github.com>
2024-12-17 16:43:46 +0100

    observe and prepare action

--- onnx_controller/include/onnx_controller/controller.hpp
+++ onnx_controller/include/onnx_controller/controller.hpp
@@ -2,6 +2,7 @@
 #include <memory>
 #include <string>
 
+#include "onnx_actor.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "unitree_go/msg/low_cmd.hpp"
@@ -11,7 +12,7 @@ constexpr std::array<uint8_t, 12> get_ros_to_urdf_idx() {
   std::array<uint8_t, 4> foot_order = {1, 0, 3, 2};
   std::array<uint8_t, 12> ros_to_urdf_idx{0};
 
-  for (size_t foot_idx = 0; foot_idx < 3; foot_idx++) {
+  for (size_t foot_idx = 0; foot_idx <= 3; foot_idx++) {
     uint8_t fid = foot_order[foot_idx];
 
     for (size_t i = 0; i < 3; i++) {
@@ -33,6 +34,8 @@ class ONNXController : public rclcpp::Node {
   // const std::array<uint8_t, 12> ros_to_urdf(const std::array<uint8_t, 12>);
 
  private:
+  void prepare_observation(std::vector<float>& observation);
+
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Subscription<unitree_go::msg::LowState>::SharedPtr subscription_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
@@ -41,12 +44,23 @@ class ONNXController : public rclcpp::Node {
   unitree_go::msg::LowState::SharedPtr last_msg_;
   unitree_go::msg::LowCmd::SharedPtr last_cmd_;
 
+  std::shared_ptr<ONNXActor> actor_;
+
   // Index conversion
   const std::array<uint8_t, 12> ros_to_urdf_idx_ = get_ros_to_urdf_idx();
 
-  // Proprioceptive state
-  std::array<float, 12> q_;           ///< Joint positions
-  std::array<float, 12> dq_;          ///< Joint velocities
+  // Inertial state
   std::array<float, 3> imu_lin_acc_;  ///< Linear acceleration
   std::array<float, 3> imu_ang_vel_;  ///< Angular velocity
+
+  std::array<float, 3> vel_cmd_{0, 0, 0};  ///< Linear velocity command
+
+  // Proprioceptive state
+  std::array<float, 12> q_;   ///< Joint positions
+  std::array<float, 12> dq_;  ///< Joint velocities
+
+  // Control state
+  std::array<float, 12> action_;  ///< Action to
+
+  // Observation state
 };
--- onnx_controller/src/controller.cpp
+++ onnx_controller/src/controller.cpp
@@ -5,20 +5,36 @@
 #include <memory>
 #include <string>
 
+#include "onnx_actor.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "unitree_go/msg/low_state.hpp"
-#include "onnx_actor.hpp"
 
 using namespace std::chrono_literals;
 
-ONNXController::ONNXController() : Node("onnx_controller"), count_(0), last_msg_(nullptr), last_cmd_(nullptr) {
+ONNXController::ONNXController()
+    : Node("onnx_controller"),
+      count_(0),
+      last_msg_(nullptr),
+      last_cmd_(nullptr) {
   subscription_ = this->create_subscription<unitree_go::msg::LowState>(
       "/lowstate", 10,
       std::bind(&ONNXController::consume, this, std::placeholders::_1));
 
   publisher_ = this->create_publisher<std_msgs::msg::String>("/lowcmd", 10);
-  timer_ = this->create_wall_timer(1000ms, std::bind(&ONNXController::publish, this));
+  timer_ = this->create_wall_timer(1000ms,
+                                   std::bind(&ONNXController::publish, this));
+
+  // Initialize the ONNX actor
+  std::string home = std::getenv("HOME");
+
+  if (home.empty()) {
+    std::cerr << "Error: HOME environment variable not set." << std::endl;
+  }
+
+  std::string model_path = home + "/.local/share/.onnx-actor/model.onnx";
+
+  actor_ = std::make_shared<ONNXActor>(model_path);
 }
 
 void ONNXController::consume(const unitree_go::msg::LowState::SharedPtr msg) {
@@ -29,15 +45,69 @@ void ONNXController::consume(const unitree_go::msg::LowState::SharedPtr msg) {
   for (size_t i = 0; i < 12; i++) {
     size_t urdf_i = ros_to_urdf_idx_[i];
     q_[urdf_i] = msg->motor_state[i].q;
-    dq_[urdf_i] = msg->motor_state[i].dq;
+    dq_[urdf_i] = 1;  // msg->motor_state[i].dq;
+
+    // Print joint positions and velocities
+    std::cout << "Joint " << i << ": q = " << q_[urdf_i]
+              << ", dq = " << dq_[urdf_i] << std::endl;
   }
+  // Check that dq_ sums to 1
+  float sum = 0;
+  for (size_t i = 0; i < 12; i++) {
+    sum += dq_[i];
+    std::cout << "idx: " << i << ", urdf_idx: " << (int)ros_to_urdf_idx_[i]
+              << std::endl;
+  }
+  std::cout << "Sum of dq_: " << sum << std::endl;
 
   // Ingest IMU data
-  imu_lin_acc_ = {msg->imu_state.accelerometer[0], msg->imu_state.accelerometer[1], msg->imu_state.accelerometer[2]};
-  imu_ang_vel_ = {msg->imu_state.gyroscope[0], msg->imu_state.gyroscope[1], msg->imu_state.gyroscope[2]};
+  imu_lin_acc_ = {msg->imu_state.accelerometer[0],
+                  msg->imu_state.accelerometer[1],
+                  msg->imu_state.accelerometer[2]};
+  imu_ang_vel_ = {msg->imu_state.gyroscope[0], msg->imu_state.gyroscope[1],
+                  msg->imu_state.gyroscope[2]};
 
   // Run the ONNX mode
+  std::vector<float> action(12, 0.0);
+  std::vector<float> observation<45, 0.0>;
+  prepare_observation(observation);
 
+  // Print observation and action
+  /*
+  std::cout << "q_: ";
+  for (const auto& q : q_) {
+    std::cout << q << " ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "Observation: ";
+  for (const auto& obs : observation) {
+    std::cout << obs << " ";
+  }
+  std::cout << std::endl;
+  */
+  actor_->update_observation(observation);
+  actor_->act(action);
+
+  /*
+  std::cout << "Action: ";
+  for (const auto& act : action) {
+    std::cout << act << " ";
+  }
+  std::cout << std::endl;
+  */
+}
+
+void prepare_observation(std::vector<float>& observation) {
+  observation.clear(); // Empty the observation vector
+  observation.insert(observation.end(), imu_lin_acc_.begin(),
+                     imu_lin_acc_.end());
+  observation.insert(observation.end(), imu_ang_vel_.begin(),
+                     imu_ang_vel_.end());
+  observation.insert(observation.end(), vel_cmd_.begin(), vel_cmd_.end());
+  observation.insert(observation.end(), q_.begin(), q_.end());
+  observation.insert(observation.end(), dq_.begin(), dq_.end());
+  observation.insert(observation.end(), action_.begin(), action_.end());
 }
 
 void ONNXController::publish() {
